ab550e1f200af15409b5ddf39f0dbe9c4c2b1b0d
Aubrey Baxton <bg548@protonmail.com>
2025-12-15 13:18:29 +0530

    pushing update

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-export const axiosInstance= axios.create({
-    baseURL: import.meta.env.MODE=== "development"? "http://localhost:8080/api/v1": "/api/v1",
+export const axiosInstance = axios.create({
+    // baseURL: import.meta.env.MODE === "development"? "http://localhost:8080/api/v1": "/api/v1",
+    baseURL: "https://leetlab-backend-qr0y.onrender.com/api/v1",
     withCredentials: true,
 })
